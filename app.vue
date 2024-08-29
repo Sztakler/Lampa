e392c7d7a85b5b3ef9f020e2688f7e86b186e114
metarth <krystian.jasionek@gmail.com>
2024-08-29 12:19:01 +0200

    Defined basic styles

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <Weather />
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    background-color: var(--light-gray);
+}
+</style>
---
+++ components/TextButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button>
+        <h4>
+            <slot></slot>
+        </h4>
+    </button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+    button {
+        all: unset;
+    }
+
+    button:hover {
+        text-decoration: underline;
+    }
+</style>
