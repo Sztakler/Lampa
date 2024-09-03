e29d656c2a4b784c741c45438e91d2a72a0738c9
metarth <krystian.jasionek@gmail.com>
2024-09-03 12:09:12 +0200

    Disabled prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-08-29",
-  devtools: { enabled: true },
-  css: ["@/assets/css/main.css"],
-  modules: ["@pinia/nuxt"],
+    compatibilityDate: "2024-08-29",
+    devtools: { enabled: true },
+    css: ["@/assets/css/main.css"],
+    modules: ["@pinia/nuxt"],
+    routeRules: {
+        "/": { prerender: false },
+    },
 });
