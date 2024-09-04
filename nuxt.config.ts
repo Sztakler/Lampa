0b5a90de7f8b7dbc9c1aa1a7b2ed10b4b7522058
metarth <krystian.jasionek@gmail.com>
2024-09-04 11:51:05 +0200

    Testing baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     css: ["@/assets/css/main.css"],
     modules: ["@pinia/nuxt"],
     app: {
-        baseURL: "/Lampa/",
+        baseURL: "https://sztakler.github.io/Lampa/",
     },
     // routeRules: {
     //     "/": { prerender: false },
