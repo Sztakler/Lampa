375c120e119cab31ef805b5208d4433351cdce60
metarth <krystian.jasionek@gmail.com>
2024-09-04 10:37:59 +0200

    Changed baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     css: ["@/assets/css/main.css"],
     modules: ["@pinia/nuxt"],
     app: {
-        baseURL: "/Lampa/",
+        baseURL: "/",
     },
     // routeRules: {
     //     "/": { prerender: false },
